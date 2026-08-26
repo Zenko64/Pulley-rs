08c94987767524b28e108a5b1a623487f6020b58
Simão Correia <simi.correia@outlook.com>
2026-08-26 11:48:57 +0100

    download webm to path

--- src-tauri/src/core/youtube.rs
+++ src-tauri/src/core/youtube.rs
@@ -3,11 +3,19 @@
 //pub struct DownloadInput {
 //};
 
+use std::path::PathBuf;
+
 #[tauri::command]
-pub fn download(url: String, path: String) -> Result<(), String> {
+pub async fn download(url: String, path: String) -> Result<(), String> {
     youtube_dl::YoutubeDl::new(url)
-        .output_directory(path)
-        .run()
+        .output_template("%(title)s.%(ext)s")
+        .format("bestvideo*+bestaudio/best")
+        .download_to(
+            PathBuf::from(path)
+                .parent()
+                .ok_or_else(|| "The provided path is invalid.".to_string())?,
+        )
         .map_err(|e| e.to_string())?;
+
     Ok(())
 }
--- src/page/App.tsx
+++ src/page/App.tsx
@@ -3,25 +3,28 @@ import { Input } from "@/components/ui/input";
 import { Progress } from "@/components/ui/progress";
 import { Download } from 'lucide-react';
 import { invoke } from '@tauri-apps/api/core';
-import { downloadDir } from '@tauri-apps/api/path';
 import { useState } from "react";
 import { toast } from "@/components/ui/toast";
+import { Spinner } from "@/components/ui/spinner";
+import { downloadDir } from '@tauri-apps/api/path';
 
 export function App() {
-    const [url, setUrl] = useState("")
+    const [url, setUrl] = useState<string>("")
+    const [isPending, setPending] = useState<boolean>(false)
 
     const download = async () => {
+        setPending(true)
         await invoke("download", { url: url, path: await downloadDir() }).then(() =>
             toast.add({ title: "Download Completed!", type: "success" })
         ).catch((e) =>
-            toast.add({ title: "Download Failed.", description: e instanceof Error ? e.message : String(e), type: "error" }))
+            toast.add({ title: "Download Failed.", description: e instanceof Error ? e.message : String(e), type: "error" })).finally(() => setPending(false))
     }
 
     return <div className="flex flex-col gap-2 p-4">
         <div className="flex flex-row gap-2">
             <Input placeholder="Enter URL" value={url} onChange={(e) => setUrl(e.target.value)}></Input>
-            <Button onClick={async () => await download()}><Download /><span>Download</span></Button>
+            <Button onClick={async () => await download()}>{isPending ? <><Spinner /><span>Downloading...</span></> : <><Download /><span>Download</span></>}</Button>
         </div>
         <Progress value={1} />
-    </div>
+    </div >
 }
